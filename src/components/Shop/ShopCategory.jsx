59ed62c6050cfed0b99448902f3e1dc980ab13a9
Petermwansa <mwansapeter800@gmail.com>
2024-05-29 23:41:27 +0300

    Add the search box and the functionality to the shop comp

--- src/components/Shop/ShopCategory.jsx
+++ src/components/Shop/ShopCategory.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Data from '../../products.json';
 
 const ShopCategory = ({filterItem, setItem, menuItems, setProducts, selectedCategory}) => {
   
     return (
-    <div>
-      
-    </div>
+    <>
+      <div className='widget-header'>
+        <h5 className='ms-2'>All Categories</h5>
+      </div>
+      <div className=''>
+        <button 
+          onClick={() => setProducts(Data)}  
+          className={`m-2 ${selectedCategory === 'All' ? 'bg-warning' : ''}`}
+          >
+          All
+        </button>
+        {
+          menuItems.map((val, id) => {
+            return (
+              <button 
+                className={`m-2 ${selectedCategory === val ? 'bg-warning' : ''}`} 
+                key={id}
+                onClick={() => filterItem(val)}>{val}</button>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 
