30cbc538c6458cbec0f6c92410e50b3e14668b42
Petermwansa <mwansapeter800@gmail.com>
2024-07-09 11:49:23 +0300

    Add some code to the login page

--- src/components/LoginSIgnup/Login.jsx
+++ src/components/LoginSIgnup/Login.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+
+
+const title = "Login";
+const socialTitle = "Login With Social Media";
+const buttonText = "Login Now";
+
+const socialList = [
+  {
+      iconName: 'icofont-facebook',
+      siteLink: '#',
+      className: 'facebook',
+  },
+  {
+      iconName: 'icofont-twitter',
+      siteLink: '#',
+      className: 'twitter',
+  },
+  {
+      iconName: 'icofont-linkedin',
+      siteLink: '#',
+      className: 'linkedin',
+  },
+  {
+      iconName: 'icofont-instagram',
+      siteLink: '#',
+      className: 'instagram',
+  },
+  {
+      iconName: 'icofont-pinterest',
+      siteLink: '#',
+      className: 'pinterest',
+  },
+]
 
 const Login = () => {
   return (
     <div>
-      login
+      Login
     </div>
   )
 }
 
-export default Login
+export default Login;
